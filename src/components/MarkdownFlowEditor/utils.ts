ea30cc720a67f3ef247ebbf89235391372dac3de
圈圈圈 <zhanghanhui2001@163.com>
2025-09-14 22:05:06 +0800

    Revert "fix: strengthen regex security against ReDoS attacks with strict limits"
    This reverts commit 786f745214e5457f24f9eaf36f82d0545a3f7474.

--- src/components/MarkdownFlowEditor/utils.ts
+++ src/components/MarkdownFlowEditor/utils.ts
@@ -15,10 +15,8 @@ import { SelectedOption } from "./types";
 import "./markdownFlowEditor.css";
 import { biliVideoUrlRegexp } from "./components/VideoInject";
 
-// Ultra-safe regex with strict character limits to prevent ReDoS attacks
-// Matches exactly: <iframe ...attributes... data-tag="video" ...more attributes...></iframe>
 const biliVideoContextRegexp =
-  /<iframe[\s\S]{1,500}?data-tag="video"[\s\S]{1,500}?data-title="([^"]{0,100})"[\s\S]{1,500}?src="([^"]{1,200})"[\s\S]{1,200}?><\/iframe>/gi;
+  /<iframe\s+[^>]*data-tag="video"[^>]*data-title="([^"]*)"[^>]*src="([^"]+)"[^>]*>[^<]*<\/iframe>/gi;
 const agiImgContextRegexp = /!\[([^\]]*)\]\(([^)]+)\)/gi;
 
 const parseContentInfo = (

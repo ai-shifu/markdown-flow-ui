126a1eabefa010100098033d0d9238e4168875d5
kunlulu <kunlulu810@gmail.com>
2025-10-16 11:24:27 +0800

    fix: export

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,3 +6,8 @@ export { default as MarkdownFlowEditor } from "./MarkdownFlowEditor";
 // Export types
 export type { ContentRenderProps } from "./ContentRender/ContentRender";
 export type { OnSendContentParams, CustomRenderBarProps } from "./types";
+export type {
+  EditMode,
+  UploadProps,
+  ImageResource,
+} from "./MarkdownFlowEditor";

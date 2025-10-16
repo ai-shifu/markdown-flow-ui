00365c9fe6df7b40ed6a86494ef665e49a106434
kunlulu <kunlulu810@gmail.com>
2025-10-16 11:10:12 +0800

    feat: export editMode

--- src/components/MarkdownFlowEditor/index.ts
+++ src/components/MarkdownFlowEditor/index.ts
@@ -2,3 +2,4 @@ import MarkdownFlowEditor from "./MarkdownFlowEditor";
 export default MarkdownFlowEditor;
 export type { UploadProps } from "./uploadTypes";
 export type { ImageResource } from "./components/ImageInject";
+export type { EditMode } from "./MarkdownFlowEditor";

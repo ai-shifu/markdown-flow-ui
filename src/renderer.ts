0a1dff578d862dc086e5ebd77f426fae66377faa
kunlulu <kunlulu810@gmail.com>
2026-01-23 17:47:33 +0800

    fix: export

--- src/renderer.ts
+++ src/renderer.ts
@@ -2,7 +2,9 @@
 import MarkdownFlow from "./components/MarkdownFlow";
 import ContentRender from "./components/ContentRender";
 import MarkdownFlowInput from "./components/ContentRender/MarkdownFlowInput";
-
+import IframeSandbox from "./components/ContentRender/IframeSandbox";
+import { RenderSegment } from "./components/ContentRender/utils/split-content";
+import { splitContentSegments } from "./components/ContentRender/utils/split-content";
 import type { MarkdownFlowProps } from "./components/MarkdownFlow/MarkdownFlow";
 import type { ContentRenderProps } from "./components/ContentRender/ContentRender";
 import type { MarkdownFlowInputProps } from "./components/ContentRender/MarkdownFlowInput";
@@ -10,8 +12,17 @@ import type {
   OnSendContentParams,
   CustomRenderBarProps,
 } from "./components/types";
+import type { IframeSandboxProps } from "./components/ContentRender/IframeSandbox";
+import type { SandboxAppProps } from "./components/ContentRender/SandboxApp";
+export default ContentRender;
 
-export { MarkdownFlow, ContentRender, MarkdownFlowInput };
+export {
+  MarkdownFlow,
+  ContentRender,
+  MarkdownFlowInput,
+  IframeSandbox,
+  splitContentSegments,
+};
 
 export type {
   OnSendContentParams,
@@ -19,4 +30,7 @@ export type {
   MarkdownFlowProps,
   ContentRenderProps,
   MarkdownFlowInputProps,
+  IframeSandboxProps,
+  SandboxAppProps,
+  RenderSegment,
 };

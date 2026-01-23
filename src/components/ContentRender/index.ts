b9799f048465dfd96d5968ba626a26fc1fb3c90a
kunlulu <kunlulu810@gmail.com>
2026-01-23 17:28:20 +0800

    feat: export iframesandbox

--- src/components/ContentRender/index.ts
+++ src/components/ContentRender/index.ts
@@ -1,5 +1,10 @@
 import ContentRender from "./ContentRender";
 export default ContentRender;
+export { default as IframeSandbox } from "./IframeSandbox";
+export type { IframeSandboxProps } from "./IframeSandbox";
+export type { SandboxAppProps } from "./SandboxApp";
+export type { RenderSegment } from "./utils/split-content";
+export { splitContentSegments } from "./utils/split-content";
 
 export { default as ContentRender } from "./ContentRender";
 export { default as MarkdownFlowInput } from "./MarkdownFlowInput";

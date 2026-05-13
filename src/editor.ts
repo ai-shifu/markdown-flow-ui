e40e81ca9e70b8b970315c4a09bd07ff12a4a160
kunlulu <kunlulu810@gmail.com>
2026-05-13 10:21:53 +0800

    feat(editor): add French locale support for MarkdownFlowEditor

--- src/editor.ts
+++ src/editor.ts
@@ -2,10 +2,11 @@
 import MarkdownFlowEditor from "./components/MarkdownFlowEditor";
 import {
   EditMode,
+  EditorLocale,
   UploadProps,
   ImageResource,
 } from "./components/MarkdownFlowEditor";
 
 export { MarkdownFlowEditor };
 
-export type { EditMode, UploadProps, ImageResource };
+export type { EditMode, EditorLocale, UploadProps, ImageResource };
